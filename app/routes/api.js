981c1845d3dbecbf2bdef4eed2378df5123c0067
Kyon <kyonGuo@163.com>
2017-12-18 20:29:14 +0800

    构建大致koa2框架

---
+++ app/routes/api.js
@@ -0,0 +1,7 @@
+const router = require('koa-router')
+
+var edit = require('../controllers/edit.js')
+var getLoginInfo = require('../controllers/getLoginInfo.js')
+var login = require('../controllers/login.js')
+
+module.exports = router
